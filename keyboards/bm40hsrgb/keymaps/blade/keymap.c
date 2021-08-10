fc9a387a810f51fcb2d82757f980a3ddcc57fee8
Rafael Garcia <bladecoder@gmail.com>
2021-08-11 00:40:10 +0200

    First iteration improvements.

--- keyboards/bm40hsrgb/keymaps/blade/keymap.c
+++ keyboards/bm40hsrgb/keymaps/blade/keymap.c
@@ -27,6 +27,22 @@ enum layers {
 #define LOWER LT(_LOWER, KC_BSPC)
 #define RAISE LT(_RAISE, KC_ENT)
 #define TABNUM LT(_NUM, KC_TAB)
+#define BKSPNUM LT(_NUM, KC_BSPC)
+
+// Tap Dance declarations
+enum {
+    TD_PASTE_UNDO,
+    TD_SAVE,
+    TD_COPY_CUT
+};
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for COPY, twice for CUT
+    [TD_COPY_CUT] = ACTION_TAP_DANCE_DOUBLE(C(KC_C), C(KC_X)),
+    [TD_PASTE_UNDO] = ACTION_TAP_DANCE_DOUBLE(C(KC_V), C(KC_Z)),
+    [TD_SAVE] = ACTION_TAP_DANCE_DOUBLE(KC_S, C(KC_S))
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -49,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * TODO: Check the arrow keys + COPY/PASTE. KC_CAPS lighting.
  */
 [_QWERTY] = LAYOUT_planck_mit(
-    TABNUM,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    TABNUM,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    BKSPNUM,
     LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    ES_NTIL, ES_ACUT,
     LSFT_T(KC_CAPS),        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    ES_COMM, ES_DOT,  ES_MINS, RSFT_T(KC_ENT),
     C(KC_C), C(KC_V),KC_LALT,KC_LGUI,   LOWER,    KC_SPC       ,   RAISE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT 
@@ -59,34 +75,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   \  |   ~  |   @  |   #  |   $  |   %  |   ^  |   &  |   |  |   (  |   )  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |   ¡  |   !  |   ¿  |   ?  |   €  |      |      |   =  |   {  |   }  |  '   |
+ * | Del  |      |      |   €  |      |      |   ¿  |   ?  |   =  |   {  |   }  |  "   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   <  |   >  |   `  |      |      |      |      |   /  |   *  |   +  |      |
+ * |      |   <  |   >  |   `  |      |      |   ¡  |   !  |   /  |   *  |   +  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      | Home | Pg Dn| Pg Up|  End |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_mit(
     ES_BSLS, ES_TILD,   ES_AT, ES_HASH,  ES_DLR, ES_PERC, ES_CIRC,   ES_AMPR,    ES_PIPE,    ES_LPRN, ES_RPRN, KC_BSPC,
-    KC_DEL,  ES_IEXL, ES_EXLM, ES_IQUE, ES_QUES, ES_EURO, XXXXXXX,   XXXXXXX,     ES_EQL,    ES_LCBR, ES_RCBR, ES_QUOT,
-    _______, ES_LABK, ES_RABK, ES_GRV,  XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,    ES_SLSH,    ES_ASTR, ES_PLUS, _______,
-    _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    KC_DEL,  XXXXXXX, XXXXXXX, ES_EURO , XXXXXXX, XXXXXXX, ES_IQUE,  ES_QUES,     ES_EQL,    ES_LCBR, ES_RCBR, ES_DQUO,
+    _______, ES_LABK, ES_RABK, ES_GRV,  XXXXXXX, XXXXXXX, ES_IEXL,  ES_EXLM,    ES_SLSH,    ES_ASTR, ES_PLUS, _______,
+    _______, _______, _______, _______, _______, _______, _______,    KC_HOME, KC_PGDN,   KC_PGUP, KC_END
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * |   /  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | Ins  | Impr |   [  |   ]  |  "   |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | Ins  | Impr |   [  |   ]  |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Home | End | Pg Up | Pg Dn|      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Home | End  | Pg Up| Pg Dn|      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_mit(
     ES_SLSH,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_INS,  KC_PSCR,  ES_LBRC, ES_RBRC, ES_DQUO,
+    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_INS,  KC_PSCR,  ES_LBRC, ES_RBRC, ES_QUOT,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HOME, KC_END,   KC_PGUP, KC_PGDN, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
@@ -96,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset|Debug | RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-|  Del |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * | C+A+D|      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -105,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_planck_mit(
     _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
+    C(A(KC_DEL)), _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 ),
@@ -135,16 +151,16 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 
 // Runs constantly in the background, in a loop.
-void matrix_scan_user(void) {
-  rgb_matrix_set_color(4, RGB_WHITE);
-  rgb_matrix_set_color(7, RGB_WHITE);
+// void matrix_scan_user(void) {
+//   rgb_matrix_set_color(4, RGB_WHITE);
+//   rgb_matrix_set_color(7, RGB_WHITE);
 
-  rgb_matrix_set_color(16, RGB_WHITE);
-  rgb_matrix_set_color(19, RGB_WHITE);
+//   rgb_matrix_set_color(16, RGB_WHITE);
+//   rgb_matrix_set_color(19, RGB_WHITE);
 
-  rgb_matrix_set_color(28, RGB_WHITE);
-  rgb_matrix_set_color(31, RGB_WHITE);
+//   rgb_matrix_set_color(28, RGB_WHITE);
+//   rgb_matrix_set_color(31, RGB_WHITE);
 
-  rgb_matrix_set_color(40, RGB_WHITE);
-  rgb_matrix_set_color(42, RGB_WHITE);
-}
+//   rgb_matrix_set_color(40, RGB_WHITE);
+//   rgb_matrix_set_color(42, RGB_WHITE);
+// }
