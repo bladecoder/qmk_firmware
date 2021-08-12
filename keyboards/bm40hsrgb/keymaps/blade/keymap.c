22e521787dd223769cf036164f2963bbb48fcc1f
Rafael Garcia <bladecoder@gmail.com>
2021-08-12 11:55:55 +0200

    Tap dance CUT and UNDO.

--- keyboards/bm40hsrgb/keymaps/blade/keymap.c
+++ keyboards/bm40hsrgb/keymaps/blade/keymap.c
@@ -41,7 +41,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap once for COPY, twice for CUT
     [TD_COPY_CUT] = ACTION_TAP_DANCE_DOUBLE(C(KC_C), C(KC_X)),
     [TD_PASTE_UNDO] = ACTION_TAP_DANCE_DOUBLE(C(KC_V), C(KC_Z)),
-    [TD_SAVE] = ACTION_TAP_DANCE_DOUBLE(KC_S, C(KC_S))
+    [TD_SAVE] = ACTION_TAP_DANCE_DOUBLE(KC_Q, C(KC_S))
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TABNUM,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    BKSPNUM,
     LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    ES_NTIL, ES_ACUT,
     LSFT_T(KC_CAPS),        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    ES_COMM, ES_DOT,  ES_MINS, RSFT_T(KC_ENT),
-    C(KC_C), C(KC_V),KC_LALT,KC_LGUI,   LOWER,    KC_SPC       ,   RAISE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT 
+    TD(TD_COPY_CUT), TD(TD_PASTE_UNDO),KC_LALT,KC_LGUI,   LOWER,    KC_SPC       ,   RAISE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT 
 ),
 
 /* Lower
