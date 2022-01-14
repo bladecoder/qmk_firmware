4e4fd498193d6283de52ad9015539e96eb591e87
Rafael Garcia <bladecoder@gmail.com>
2022-01-14 18:20:23 +0100

    Improve ¡ and ¿

--- keyboards/kprepublic/bm40hsrgb/keymaps/blade/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/keymaps/blade/keymap.c
@@ -119,17 +119,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   /  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |      |   [  |   ]  |  '   |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |   ¿  |   [  |   ]  |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |   <  |   >  |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |   ¡  |   <  |   >  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |SHome |SPg Dn|SPg Up| SEnd |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_mit(
     ES_SLSH, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   XXXXXXX, XXXXXXX,  ES_LBRC, ES_RBRC, ES_QUOT,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX,  ES_LABK, ES_RABK, _______,
+    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   XXXXXXX, ES_QUES,  ES_LBRC, ES_RBRC, ES_QUOT,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, ES_EXLM,  ES_LABK, ES_RABK, _______,
     _______, _______, _______, _______, _______, _______, _______, S(KC_HOME), S(KC_PGDN),   S(KC_PGUP), S(KC_END)
 ),
 
@@ -156,17 +156,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * ,-----------------------------------------------------------------------------------.
   * |      |  Next| Vol- | Vol+ | Play |      |      |   7  |   8  |   9  |   *  | Bksp |
   * |------+------+------+------+------+-------------+------+------+------+------+------|
-  * |      |SPrtSc| PrtSc|  Ins |      |      | Bksp |   4  |   5  |   6  |   +  |  Del |
+  * |      |SPrtSc| PrtSc|  Ins | Next | Play | Bksp |   4  |   5  |   6  |   +  |  Del |
   * |------+------+------+------+------+------|------+------+------+------+------+------|
-  * |      | Desk1| Desk2|      |      |      | Enter|   1  |   2  |   3  |   -  |Enter |
+  * |      | Desk1| Desk2| Vol- | Vol+ |      | Enter|   1  |   2  |   3  |   -  |Enter |
   * |------+------+------+------+------+------+------+------+------+------+------+------|
   * | RCtl | RAlt |      |      |      |             |   0  |   .  |   ,  |   /  |  =   |
   * `-----------------------------------------------------------------------------------'
   */
 [_NUM] = LAYOUT_planck_mit(
         _______ , KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  , XXXXXXX   , XXXXXXX,  KC_P7,   KC_P8, KC_P9,   KC_PAST, KC_BSPC,
-        _______ , G(S(KC_S))   , KC_PSCR   , KC_INS   , XXXXXXX   , XXXXXXX , KC_BSPC, KC_P4, KC_P5, KC_P6,   KC_PPLS, KC_DEL,
-        _______ , C(G(KC_LEFT))   , C(G(KC_RGHT))  , XXXXXXX  , XXXXXXX  , XXXXXXX , KC_PENT, KC_P1, KC_P2, KC_P3,   KC_PMNS, KC_PENT,
+        _______ , G(S(KC_S))   , KC_PSCR   , KC_INS   , KC_MNXT   , KC_MPLY , KC_BSPC, KC_P4, KC_P5, KC_P6,   KC_PPLS, KC_DEL,
+        _______ , C(G(KC_LEFT))   , C(G(KC_RGHT))  , KC_VOLD  , KC_VOLU  , XXXXXXX , KC_PENT, KC_P1, KC_P2, KC_P3,   KC_PMNS, KC_PENT,
         KC_RCTRL , KC_RALT , _______ , _______ , _______ ,      XXXXXXX     , KC_P0, KC_PDOT, ES_COMM, KC_PSLS, ES_EQL
         )
 
